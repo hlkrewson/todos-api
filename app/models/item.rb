378ceaa633aa3edaaed8e2b45e70a6c9fd2c4314
Harry Krewson <harrykrewson@gmail.com>
2017-07-12 11:23:11 -0600

    Add controllers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :todo
+	#model association
+  	belongs_to :todo
+
+  	# validation
+  	validates_presence_of :name
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	# Association test
+	# ensure an item record belongs to a single todo record
+	it { should belong_to(:todo) }
+	# validation test
+	# ensure column name is present before savings
+	it { should validate_presence_of(:name) }
 end
